c6be8b0f4aa5e803376b454c082d3d032033caf7
hovelopin <kimhojin3714@naver.com>
2024-04-10 23:39:12 +0900

    feat : service , controller 작업 완료

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,23 +1,32 @@
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
 import { CreateMessageDto } from './dtos/create-message.dto';
+import { MessagesService } from './messages.service';
 
 // class decorator
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+
+  constructor() {
+    // DONT DO THIS ON REAL APP
+    this.messagesService = new MessagesService();
+  }
   // method decorator
   @Get()
   listMessages() {
-    console.log('hi');
+    return this.messagesService.findAll();
   }
 
   // Body , Param => argument decorator
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
     console.log('body', body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessage(@Param('id') id: string) {
     console.log('id', id);
+    return this.messagesService.findOne(id);
   }
 }
